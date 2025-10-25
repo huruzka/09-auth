452f7626407638c9f7e6a6747abbe09235f216a1
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-10-25 23:22:46 +0300

    Update api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -3,20 +3,8 @@ import axios from "axios";
 // 🔹 Створюємо axios-інстанс
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_NOTEHUB_URL,
-  withCredentials: true, // 👈 обов’язково для передачі cookie
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
+  withCredentials: true, //  для передачі cookie
 });
 
-// 🔹 Перехоплювачі для логів (опціонально, щоб бачити помилки)
-apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error("API error:", error.response || error.message);
-    throw error;
-  }
-);
 
 export default apiClient;
